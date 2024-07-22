e3cc759b1737364dbf11875fd9e70da7fd8edd8c
JesusAlbertoTovar <tovar25072005@gmail.com>
2024-07-22 15:00:32 -0600

    el inicio de todo

---
+++ src/main.js
@@ -0,0 +1,26 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import { createVuetify } from 'vuetify'
+
+import App from './App.vue'
+import router from './router'
+import 'vuetify/styles'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.cs'
+
+const app = createApp(App)
+const vuetify = createVuetify({
+    components,
+    directives,
+    icons: {
+      iconfont: 'mdi', 
+    },
+  })
+  app.use(vuetify)
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
