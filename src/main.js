dcf2e9c7ad968f06dd67d05b2bce6108ba486ed1
JesusAlbertoTovar <tovar25072005@gmail.com>
2024-07-23 10:39:59 -0600

    segundo cambio

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import router from './router'
 import 'vuetify/styles'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
-import '@mdi/font/css/materialdesignicons.cs'
+
 
 const app = createApp(App)
 const vuetify = createVuetify({
