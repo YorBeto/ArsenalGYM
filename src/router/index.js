3c8723feca4fe13921ec760993a0558a6c23e4d1
JonathanRios18 <sikra18@gmail.com>
2024-07-24 16:50:09 -0600

    elimine el dashboard

--- src/router/index.js
+++ src/router/index.js
@@ -51,11 +51,6 @@ const router = createRouter({
       name: 'Registro',
       component: RegistroView
     },
-    {
-      path: '/Dashboard',
-      name: 'Dashboard',
-      component: DashboardView
-    },
   ]
 })
 
