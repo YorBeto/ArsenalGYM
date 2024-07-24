277c70df52bfae9031020944f76bd26d7639f249
avinautt12 <avinaluciano50@gmail.com>
2024-07-24 13:35:39 -0600

    ajuste cambios barra de admin (botones)

--- src/components/BarralateralAdmin.vue
+++ src/components/BarralateralAdmin.vue
@@ -1,19 +1,47 @@
 <template>
     <div>
         <aside class="sidebar">
-        <v-btn class="botones">Clientes</v-btn>
+        <v-btn class="botones" @mouseover="activarColorGris()" @mouseleave="restaurarColorboton()" :color="coloresBotones1">Clientes</v-btn>
         <v-btn class="botones">Socios</v-btn>
         <v-btn class="botones">Citas</v-btn>
         <v-btn class="botones">Productos</v-btn>
         <v-btn class="botones">Avisos</v-btn>
-        <v-btn class="botonSalir" color="red">Salir</v-btn>
+        <v-btn class="botonSalir" @mouseover="activarColorRojo()" @mouseleave="restaurarColor()" :color="colorSalir">Salir</v-btn>
       </aside>
     </div>
 </template>
 
 
 <script setup>
+import { ref } from 'vue';
 
+const coloresBotones1 = {
+  clientes: 'grey',
+  socios: 'grey',
+  citas: 'grey',
+  productos: 'grey',
+  avisos: 'grey'
+};
+
+const coloresBoton = ref('grey');
+
+function activarColorGris() {
+  coloresBoton.value = 'grey';
+}
+
+function restaurarColorboton() {
+  coloresBoton.value = 'white';
+}
+
+const colorSalir = ref('red');
+
+function activarColorRojo() {
+  colorSalir.value = 'red';
+}
+
+function restaurarColor() {
+  colorSalir.value = 'white';
+}
 </script>
 
 <style scoped>
@@ -39,6 +67,16 @@
   background-color: red;
   border-color: red;
   color: white;
+  text-transform: capitalize;
 }
 
+.botonSalir:hover {
+  background-color: red !important;
+  color: white !important;
+}
+
+.botones:hover {
+  background-color: grey !important;
+  color: white !important;
+}
 </style>
