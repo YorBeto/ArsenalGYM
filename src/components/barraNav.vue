09c248fd613a59b198f6e26d2c805dd1e452ed78
daniboy30 <garciaazamardaniel@gmail.com>
2024-07-26 10:47:04 -0600

    cambios pequeños

--- src/components/barraNav.vue
+++ src/components/barraNav.vue
@@ -37,28 +37,28 @@
 
 <style scoped>
 .imagen-izquierda {
-  width: 150px; /* Ajusta el ancho según sea necesario */
-  height: auto; /* Mantiene la proporción de la imagen */
-  cursor: pointer; /* Cambia el cursor al pasar sobre la imagen si es clickeable */
-  margin-left: 16px; /* Espaciado a la izquierda */
+  width: 150px; 
+  height: auto; 
+  cursor: pointer; 
+  margin-left: 16px; 
 }
 
 .nav-buttons {
   display: flex;
-  gap: 10px; /* Espaciado entre botones */
+  gap: 10px; 
   justify-content: center;
   align-items: center;
 }
 
 .boton-bar {
   color: white;
-  background-color: transparent; /* Fondo transparente para botones */
-  font-size: 20px; /* Tamaño de fuente */
-  font-family: Arial, Helvetica, sans-serif; /* Fuente del botón */
+  background-color: transparent;
+  font-size: 20px;
+  font-family: Arial, Helvetica, sans-serif; 
 }
 
 .boton-bar:hover {
-  background-color: rgba(255, 255, 255, 0.1); /* Color de fondo en hover */
+  background-color: rgba(255, 255, 255, 0.1); 
 }
 
 .boton-compras .v-icon,
@@ -83,10 +83,10 @@
   background-color: transparent; 
 }
 
-/* Agrupación de los botones en la derecha */
+
 .right-buttons {
   display: flex;
-  gap: 8px; /* Ajusta el espaciado entre los botones */
+  gap: 8px; 
 }
 </style>
 
