2f943bf90f03c0b9eddd31b76b67b616a37f82fb
avinautt12 <avinaluciano50@gmail.com>
2024-07-29 16:50:40 -0600

    "cambio index"

--- src/router/index.js
+++ src/router/index.js
@@ -12,6 +12,8 @@ import PostcompraView from '@/views/PostcompraView.vue'
 import AdminClientesView from '@/views/AdminClientesView.vue'
 import PerfilUsuarioView from '@/views/PerfilUsuarioView.vue'
 import AdminAgregarProductoView from '@/views/AdminAgregarProductoView.vue'
+import InicioClienteView from '@/views/InicioClienteView.vue'
+import AdminEditarProductoView from '@/views/AdminEditarProductoView.vue'
 
 
 
