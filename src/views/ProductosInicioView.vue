4ad69e6730b76924b7160e47eb03160402f73723
daniboy30 <garciaazamardaniel@gmail.com>
2024-07-30 13:05:17 -0600

    llamar paginas de productos

--- src/views/ProductosInicioView.vue
+++ src/views/ProductosInicioView.vue
@@ -1,66 +1,50 @@
+<script setup>
+import { ref } from 'vue';
+import barraNav from '@/components/barraNav.vue';
+import PreEntrenos from '@/views/Pre-entrenosView.vue';
+import Proteinas from '@/views/ProteinasView.vue';
+import Creatinas from '@/views/CreatinaView.vue';
+import Multivitaminicos from '@/views/MultivitanimicosView.vue';
+import Membresias from '@/views/membresiasView.vue';
+const categories = ref([ 
+  'PRE-ENTRENOS',
+  'PROTEÍNAS',
+  'CREATINAS',
+  'MULTIVITAMÍNICOS',
+  'MEMBRESÍAS' 
+]);
+const tab = ref(0); 
+</script>
+
 <template>
   <v-layout class="rounded rounded-md" style="background-color: #f0f0f0; min-height: 100vh;">
     <barraNav></barraNav>
-
     <v-main>
       <v-container>
-        <v-tabs class="centered-tabs" background-color="#ffffff" slider-color="red">
+        <v-tabs v-model="tab" class="centered-tabs" background-color="black" slider-color="red">
           <v-tab v-for="category in categories" :key="category">{{ category }}</v-tab>
         </v-tabs>
-
-        <v-tabs-items>
-          <v-tab-item v-for="category in categories" :key="category">
-            <v-row>
-              <v-col v-for="product in getProductsByCategory(category)" :key="product.id" cols="12" md="4">
-                <v-card class="my-4">
-                  <v-img :src="product.image" class="white--text" height="200px"></v-img>
-                  <v-card-title>{{ product.name }}</v-card-title>
-                  <v-card-text>{{ product.description }}</v-card-text>
-                </v-card>
-              </v-col>
-            </v-row>
+        <v-tabs-items v-model="tab">
+          <v-tab-item v-if="tab === 0"> 
+            <pre-entrenos />
+          </v-tab-item>
+          <v-tab-item v-if="tab === 1">
+            <proteinas />
+          </v-tab-item>
+          <v-tab-item v-if="tab === 2">
+            <creatinas />
+          </v-tab-item>
+          <v-tab-item v-if="tab === 3">
+            <multivitaminicos />
+          </v-tab-item>
+          <v-tab-item v-if="tab === 4">
+            <membresias />
           </v-tab-item>
         </v-tabs-items>
       </v-container>
     </v-main>
   </v-layout>
 </template>
-
-<script setup>
-import { ref, onMounted } from 'vue';
-import barraNav from '@/components/barraNav.vue';
-
-const categories = ref([
-  'PRE-ENTRENOS',
-  'PROTEÍNAS',
-  'CREATINAS',
-  'MULTIVITAMÍNICOS',
-  'MEMBRESÍAS'
-]);
-
-const products = ref([]);
-
-const mostrarproductos = () => {
-  fetch('http://mipagina.com/productos')
-    .then(response => response.json())
-    .then(json => {
-      if (json.status === 200) {
-        products.value = json.data;
-      }
-    })
-    .catch(error => {
-      console.error('Error fetching products:', error);
-    });
-};
-
-onMounted(() => {
-  mostrarproductos();
-});
-
-function getProductsByCategory(category) {
-  return products.value.filter(product => product.category === category);
-}
-</script>
 
 <style scoped>
 .centered-tabs {
