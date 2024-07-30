00f8f054d8bb97d1384504fa6a2536033c228e07
avinautt12 <avinaluciano50@gmail.com>
2024-07-30 12:25:59 -0600

    COMPONENTE BARRA ADMIN SUPERIOR

--- src/views/AdminCitasView.vue
+++ src/views/AdminCitasView.vue
@@ -1,8 +1,6 @@
 <template>
     <div id="admin-inicio">
-      <nav class="navbar">
-        <img src="/public/arsenal.png" class="logo" />
-      </nav>
+        <BarraAdminNew></BarraAdminNew>
       <div class="contenedor">
         <BarralateralAdmin></BarralateralAdmin>
         <div class="main-content">
@@ -29,7 +27,8 @@
   
   <script setup>
   
-  import BarralateralAdmin from '@/components/BarralateralAdmin.vue';
+  import BarraAdminNew from '@/components/BarraAdminNew.vue';
+import BarralateralAdmin from '@/components/BarralateralAdmin.vue';
   import {ref,onMounted} from 'vue';
   const search = ref('')
   
