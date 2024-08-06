3efaec1db60841b106cb441d5d6d30846b5c4e8f
avinautt12 <avinaluciano50@gmail.com>
2024-08-06 13:13:25 -0600

    inicio

--- src/components/barraNav.vue
+++ src/components/barraNav.vue
@@ -1,43 +1,60 @@
 <template>
   <v-app-bar color="black" app style="height: 70px; font-size: 24px; display: flex; align-items: center; position: fixed;">
-    <v-img src="/arsenal.png" alt="Logo" contain class="imagen-izquierda" @click="scrollToTop"></v-img>
-    <v-spacer></v-spacer>
-    <div class="nav-buttons"> 
-      <router-link to="/"><v-btn class="boton-bar">INICIO</v-btn></router-link>
-      <router-link to="/Producto"><v-btn class="boton-bar">PRODUCTOS</v-btn></router-link>
-      <router-link to="Inbody"><v-btn class="boton-bar">INBODY</v-btn></router-link>
-    </div>
-    <v-spacer></v-spacer>
-    <router-link to="/Login">
-      <v-btn icon class="boton-bar boton-iniciar-sesion">
-        <v-icon>mdi-account</v-icon>
-      </v-btn> 
-    </router-link>
-    <router-link to="/Compras">
-      <v-btn icon class="boton-bar boton-compras">
-        <v-icon>mdi-cart</v-icon>
-      </v-btn>
-    </router-link>
+    <v-container fluid>
+      <v-row class="but" >
+        <v-col cols="auto">
+          <v-img src="/arsenal.png" alt="Logo" contain class="imagen-izquierda" @click="scrollToTop"></v-img>
+        </v-col>
+
+        <v-col cols="auto">
+          <v-row no-gutters class="nav-buttons">
+            <v-col cols="auto">
+              <router-link to="/"><v-btn class="boton-bar">INICIO</v-btn></router-link>
+            </v-col>
+            <v-col cols="auto">
+              <router-link to="/Producto"><v-btn class="boton-bar">PRODUCTOS</v-btn></router-link>
+            </v-col>
+            <v-col cols="auto">
+              <router-link to="Inbody"><v-btn class="boton-bar">INBODY</v-btn></router-link>
+            </v-col>
+          </v-row>
+        </v-col>
+
+        <v-col cols="auto">
+          <v-row no-gutters>
+            <v-col cols="auto">
+              <router-link to="/Login">
+                <v-btn icon class="boton-bar boton-iniciar-sesion">
+                  <v-icon>mdi-account</v-icon>
+                </v-btn>
+              </router-link>
+            </v-col>
+            <v-col cols="auto">
+              <router-link to="/Compras">
+                <v-btn icon class="boton-bar boton-compras">
+                  <v-icon>mdi-cart</v-icon>
+                </v-btn>
+              </router-link>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-app-bar>
-</template> 
+</template>
 
 <style scoped>
-
-
+.but{
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
 
 .imagen-izquierda {
-  width: 150px; /* Ajusta el ancho según sea necesario */
+  width: 70px; /* Ajusta el ancho según sea necesario */
   height: auto; /* Mantiene la proporción de la imagen */
   cursor: pointer; /* Cambia el cursor al pasar sobre la imagen si es clickeable */
-  margin-left: 16px; /* Espaciado a la izquierda */
-}
- 
-.nav-buttons {
-  display: flex;
-  gap: 16px; 
-  justify-content: center;
-  align-items: center;
-
+  margin-left: 210px; /* Espaciado a la izquierda */
 }
 
 .boton-bar {
@@ -46,13 +63,16 @@
   min-width: 120px;
   font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif; 
   text-align: center;
+  margin-left: 5px; /* Espaciado a la izquierda */
 }
 
 
 .boton-iniciar-sesion, .boton-compras {
-  margin-right: 40px;
+  margin-right: 4px;
+  gap: 4px;
   color: white;
 }
+
 </style>
 
 <script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -16,73 +16,74 @@ const items = ref([
     <barraNav></barraNav>
 
     <v-main style="background: linear-gradient(to right, black, red); display: flex; flex-direction: column; align-items: center; padding: 20px;">
-      <v-carousel class="carousel mb-5" cycle interval="5000" hide-delimiter-background height="50/0px">
-        <v-carousel-item v-for="(item, i) in items" :key="i">
-          <v-img :src="item.src" :alt="item.alt" class="carousel-image"></v-img>
-        </v-carousel-item>
-      </v-carousel>
+      <v-container fluid>
+        <!-- Carousel -->
+        <v-row class="mb-5">
+          <v-col cols="12">
+            <v-carousel class="carousel" cycle interval="5000" hide-delimiter-background>
+              <v-carousel-item v-for="(item, i) in items" :key="i">
+                <v-img :src="item.src" :alt="item.alt" class="carousel-image"></v-img>
+              </v-carousel-item>
+            </v-carousel>
+          </v-col>
+        </v-row>
 
-      <div class="inbody-section mb-5">
-        <v-img src="/INB270.png" alt="InBody Image" class="imagen-inbody"></v-img>
-        <div class="inbody-text">
-          <h2 class="mb-4">EN ARSENAL NOS INTERESA TU SALUD</h2>
-          <p>
-            InBody es líder mundial en tecnología de análisis de composición corporal. A través de nuestros innovadores dispositivos, proporcionamos una evaluación precisa y detallada de los componentes del cuerpo humano, incluyendo masa muscular, grasa corporal, agua corporal total y mucho más. Nuestros equipos son utilizados por profesionales de la salud, entrenadores personales y centros de fitness para ayudar a individuos a comprender mejor su cuerpo y mejorar su bienestar.
-          </p>
-          <br>
-          <p>Conoce tu cuerpo con nuestro InBody, una máquina capaz de medir las características de tu cuerpo. Agenda tu cita para descubrir tus límites.</p>
-          <router-link to="Inbody">
-            <v-btn class="boton-agendar mt-4">AGENDAR</v-btn>
-          </router-link>
-        </div>
-      </div>
+        <v-row class="mb-5">
+          <v-col cols="12">
+            <v-card class="cartainfo d-flex flex-row">
+              <!-- Texto -->
+              <v-card-text class="text-left gym-text-container">
+                <div class="gym-text2">
+                  <p>
+                    En Arsenal tenemos todo lo que necesitas y mucho más para cada entrenamiento.
+                    Tenemos nuevas áreas con mejores y aparatos más especializados para ayudarte a obtener los resultados que buscas.
+                  </p>
+                </div>
+              </v-card-text>
+              
+              <!-- Imagen -->
+              <v-img
+                src="/inicioluz.jpeg"
+                alt="inicio-image"
+                class="imagen-inicio"
+              ></v-img>
+            </v-card>
+          </v-col>
+        </v-row>
+           
+        <v-row class="mb-5">
+          <v-col cols="12">
+            <v-card class="cartainfo d-flex flex-row">
+              <v-img
+                src="/Avina.jpg"
+                alt="Arsenal Image"
+                class="imagen-avina"
+              ></v-img>
+              <!-- Texto -->
+              <v-card-text class="d-flex align-center flex-grow-1">
+                <div class="gym-text">
+                  <p>
+                    En Arsenal somos un gimnasio que busca el bienestar de nuestros socios como también superar sus límites. Ofrecemos variedad de equipo, instructores y también contamos con clases de crossfit.
+                  </p>
+                </div>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
 
-      <div class="gym-section mb-5">
-        <v-img src="/Avina.jpg" alt="Arsenal Image" class="imagen-arsenal"></v-img>
-        <div class="gym-text">
-          <p>En Arsenal somos un gimnasio que busca el bienestar de nuestros socios como también superar sus límites. Ofrecemos variedad de equipo, instructores y también contamos con clases de crossfit.</p>
-        </div>
-      </div>
-      
-     
-      <div class="membresias-section mb-5">
-        <h2>MEMBRESÍAS</h2>
-        <div class="membresias-table">
-          <table>
-            <thead>
-              <tr>
-                <th></th>
-                <th>MENSUAL</th>
-                <th>SEMESTRAL</th>
-                <th>ANUAL</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>GYM</td>
-                <td>$700</td>
-                <td>$1,800</td>
-                <td>$7,460</td>
-              </tr>
-              <tr>
-                <td>CROSSFIT</td>
-                <td>$720</td>
-                <td>$1,920</td>
-                <td>$7,550</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </div>
-
- 
-      <div class="visitanos-section">
-        <h2>VISÍTANOS</h2>
-        <div class="visitanos-content">
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3599.8266233107443!2d-103.43089429999999!3d25.5441515!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x868fdb933efabea1%3A0x4ff88cb25a0f6b13!2sGimnasio%20Arsenal%20Bosque!5e0!3m2!1ses!2smx!4v1721764484976!5m2!1ses!2smx" class="mapa-arsenal-gym"></iframe>
-          <v-img src="/gymfuera.png" alt="Arsenal Gym" class="imagen-arsenal-gym"></v-img>
-        </div>
-      </div>
+        <!-- Visítanos Section -->
+        <v-row class="mb-5">
+          <v-col cols="12">
+            <div class="visitanos-section">
+              <h2>VISÍTANOS</h2>
+              <div class="visitanos-content">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3599.8266233107443!2d-103.43089429999999!3d25.5441515!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x868fdb933efabea1%3A0x4ff88cb25a0f6b13!2sGimnasio%20Arsenal%20Bosque!5e0!3m2!1ses!2smx!4v1721764484976!5m2!1ses!2smx" class="mapa-arsenal-gym" style="width: 100%; height: 450px; border: 0;" allowfullscreen="" loading="lazy"></iframe>
+                <v-img src="/gymfuera.png" alt="Arsenal Gym" class="imagen-arsenal-gym"></v-img>
+              </div>
+            </div>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-main>
   </v-layout>
 </template>
@@ -96,28 +97,128 @@ const items = ref([
   padding: 16px; 
 }
 
-.imagen-avina{
-  flex: 1 1 40%; /* Permite que la imagen ocupe el 50% del ancho del card */
-  margin-left: 95px;
+.cartainfo {
+  display: flex;
+  flex-direction: row; /* Alinea los elementos en una fila */
+  align-items: flex-start; /* Alinea el contenido en la parte superior del contenedor */
+  padding: 16px; /* Espaciado alrededor del contenido */
 }
 
-.imagen-inicio{
-  flex: 1 1 40%; /* Permite que la imagen ocupe el 50% del ancho del card */
-  height: 490px;
-  margin-left: 115px;
-}
-
-.gym-text {
-  flex: 1 1 50%; /* Permite que el texto ocupe el 50% restante del card */
-  padding: 16px; /* Espaciado alrededor del texto */
-  box-sizing: border-box; /* Incluye el padding en el ancho total */
+.gym-text-container {
+  flex: 15; /* Permite que el texto ocupe el espacio disponible */
+  padding-right: 6px; /* Espaciado entre el texto y la imagen */
 }
 
 .gym-text2 {
+  flex: 1 1 50%; 
+  margin-top: 190px;
+  padding-left: 90px;
+}
+
+.gym-text{
   flex: 1 1 50%; /* Permite que el texto ocupe el 50% restante del card */
   padding: 16px; /* Espaciado alrededor del texto */
   box-sizing: border-box; /* Incluye el padding en el ancho total */
-  margin-left: 100px
+  margin-top: 160px;
+}
+
+.imagen-inicio {
+  flex: 1 1 40%; /* Ajusta el ancho de la imagen según sea necesario */
+  height: auto; /* Mantiene la proporción de la imagen */
+}
+
+.imagen-avina{
+  flex: 1 1 40%; /* Permite que la imagen ocupe el 50% del ancho del card */
+  margin-left: 15px;
+}
+
+/* Responsive Styles */
+@media (max-width: 600px) {
+  .carousel {
+    height: 300px; /* Reduce la altura del carrusel para pantallas pequeñas */
+  }
+
+  .carousel-image {
+    height: 300px; /* Ajusta la altura de las imágenes del carrusel */
+  }
+
+  /* Ajustes para el primer card */
+  .imagen-inicio {
+    width: 100%; /* Ocupa el ancho completo del contenedor */
+    height: auto; /* Mantiene la proporción de la imagen */
+    margin-left: 0; /* Elimina el margen izquierdo en pantallas pequeñas */
+    margin-top: 10px; /* Ajusta el margen superior si es necesario */
+    display: flex;
+    flex-direction: column;
+  }
+
+  .gym-text-container {
+    flex: 1 1 100%; /* El texto ocupa el 100% del ancho */
+    padding-right: 0; /* Elimina el espaciado derecho en pantallas pequeñas */
+    padding-left: 0; /* Elimina el espaciado izquierdo en pantallas pequeñas */
+  }
+
+  .gym-text2 {
+    margin-top: 10px; /* Ajusta el margen superior para pantallas pequeñas */
+    padding-left: 0; /* Elimina el padding izquierdo en pantallas pequeñas */
+    text-align: center; /* Centra el texto en pantallas pequeñas */
+  }
+
+  /* Ajustes para el segundo card */
+  .imagen-avina {
+    width: 100%; /* Ocupa el ancho completo del contenedor */
+    height: auto; /* Mantiene la proporción de la imagen */
+    margin-left: 0; /* Elimina el margen izquierdo en pantallas pequeñas */
+    margin-top: 10px; /* Ajusta el margen superior si es necesario */
+  }
+
+  .gym-text {
+    flex: 1 1 100%; /* El texto ocupa el 100% del ancho del card */
+    padding: 6px; /* Añade padding para espaciado alrededor del texto */
+    text-align: center; /* Centra el texto en pantallas pequeñas */
+  }
+
+  /* Ajustes para la sección 'Visítanos' */
+  .visitanos-content {
+    flex-direction: column; /* Alinea los elementos verticalmente en pantallas pequeñas */
+    align-items: center; /* Centra los elementos horizontalmente */
+    margin-top: 20px; /* Ajusta el margen superior si es necesario */
+  }
+
+  .mapa-arsenal-gym {
+    width: 100%; /* Ocupa el ancho completo del contenedor */
+    height: 250px; /* Ajusta la altura del mapa para pantallas pequeñas */
+  }
+
+  .imagen-arsenal-gym {
+    width: 100%; /* Ocupa el ancho completo del contenedor */
+    height: auto; /* Mantiene la proporción de la imagen */
+    margin-top: 20px; /* Ajusta el margen superior si es necesario */
+  }
+
+  /* Ajustes generales para flex containers */
+  .d-flex {
+    flex-direction: column; /* Cambia la dirección del flex para pantallas pequeñas */
+  }
+
+  .cartainfo {
+    flex-direction: column; /* Cambia la dirección del flex para pantallas pequeñas */
+    align-items: center; /* Centra los elementos en pantallas pequeñas */
+  }
+
+  .cartainfo2 {
+    width: 100%;
+    padding: 16px;
+  }
+}
+
+.d-flex {
+    flex-direction: column;
+  }
+
+.imagen-inicio{
+  height: 490px;
+  margin-left: 115px;
 }
 
 .carousel {
@@ -214,3 +315,4 @@ const items = ref([
 }
 
 </style>
+
