b8dd5245c47e8e2b0a20571492e1c9bc3c44e8c7
avinautt12 <avinaluciano50@gmail.com>
2024-08-06 17:24:37 -0600

    responsiva

--- src/components/barraNav.vue
+++ src/components/barraNav.vue
@@ -1,26 +1,44 @@
 <template>
-  <v-app-bar color="black" app style="height: 70px; font-size: 24px; display: flex; align-items: center; position: fixed;">
+  <v-app-bar color="black" app dense>
     <v-container fluid>
-      <v-row class="but" >
-        <v-col cols="auto">
-          <v-img src="/arsenal.png" alt="Logo" contain class="imagen-izquierda" @click="scrollToTop"></v-img>
+      <v-row align="center" justify="space-between">
+        <v-col cols="auto" class="d-flex align-center">
+          <v-img
+            src="/arsenal.png"
+            alt="Logo"
+            contain
+            class="imagen-izquierda"
+            @click="scrollToTop"
+          ></v-img>
         </v-col>
 
-        <v-col cols="auto" >
-          <v-row no-gutters class="nav-buttons">
+        <v-col class="d-none d-md-flex" cols="auto">
+          <v-row no-gutters>
             <v-col cols="auto">
-              <router-link to="/"><v-btn class="boton-bar">INICIO</v-btn></router-link>
+              <router-link to="/">
+                <v-btn class="boton-bar">INICIO</v-btn>
+              </router-link>
             </v-col>
-            <v-col>
-              <router-link to="/Producto"><v-btn class="boton-bar">PRODUCTOS</v-btn></router-link>
+            <v-col cols="auto">
+              <router-link to="/Producto">
+                <v-btn class="boton-bar">PRODUCTOS</v-btn>
+              </router-link>
             </v-col>
-            <v-col>
-              <router-link to="Inbody"><v-btn class="boton-bar">INBODY</v-btn></router-link>
+            <v-col cols="auto">
+              <router-link to="/Inbody">
+                <v-btn class="boton-bar">INBODY</v-btn>
+              </router-link>
             </v-col>
           </v-row>
         </v-col>
 
-        <v-col cols="auto">
+        <v-col class="d-flex d-md-none" cols="auto">
+          <v-btn icon @click="toggleMenu">
+            <v-icon>mdi-menu</v-icon>
+          </v-btn>
+        </v-col>
+
+        <v-col cols="auto" class="d-flex justify-end">
           <v-row no-gutters>
             <v-col cols="auto">
               <router-link to="/Login">
@@ -40,49 +58,94 @@
         </v-col>
       </v-row>
     </v-container>
+
+    <v-menu
+      v-model="menuVisible"
+      offset-y
+      absolute
+      top
+      transition="slide-x-reverse-transition"
+    >
+      <v-list>
+        <v-list-item link :to="{ path: '/' }">
+          <v-list-item-title>INICIO</v-list-item-title>
+        </v-list-item>
+        <v-list-item link :to="{ path: '/Producto' }">
+          <v-list-item-title>PRODUCTOS</v-list-item-title>
+        </v-list-item>
+        <v-list-item link :to="{ path: '/Inbody' }">
+          <v-list-item-title>INBODY</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
   </v-app-bar>
 </template>
 
 <style scoped>
-.but{
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-}
-
 .imagen-izquierda {
-  width: 70px; /* Ajusta el ancho según sea necesario */
+  width: 50px; /* Ajusta el ancho según sea necesario */
   height: auto; /* Mantiene la proporción de la imagen */
   cursor: pointer; /* Cambia el cursor al pasar sobre la imagen si es clickeable */
-  margin-left: 210px; /* Espaciado a la izquierda */
 }
 
 .boton-bar {
   color: white;
-  font-size: 20px;
-  min-width: 120px;
-  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif; 
+  font-size: 16px;
+  min-width: 100px;
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
   text-align: center;
-  margin-left: 5px; /* Espaciado a la izquierda */
 }
 
-
-.boton-iniciar-sesion, .boton-compras {
-  margin-right: 4px;
-  gap: 4px;
-  color: white;
+.boton-iniciar-sesion,
+.boton-compras {
+  margin-left: 5px;
 }
 
+.v-menu__content {
+  min-width: 200px;
+}
+
+.d-md-flex {
+  display: flex;
+}
+
+.d-none {
+  display: none;
+}
+
+@media (max-width: 960px) {
+  .d-md-flex {
+    display: none;
+  }
+  .d-none {
+    display: flex;
+  }
+  .imagen-izquierda {
+    width: 40px; /* Ajusta el ancho para pantallas más pequeñas */
+  }
+  .boton-bar {
+    font-size: 14px;
+    min-width: 80px;
+  }
+}
 </style>
 
 <script>
 export default {
+  data() {
+    return {
+      menuVisible: false
+    };
+  },
   methods: {
     scrollToTop() {
       window.scrollTo({
         top: 0,
         behavior: 'smooth'
       });
+    },
+    toggleMenu() {
+      this.menuVisible = !this.menuVisible;
     }
   }
 };
