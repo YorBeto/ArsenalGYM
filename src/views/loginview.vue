92add0a2d430e8f18668a150dbf69635aa122a04
JesusAlbertoTovar <tovar25072005@gmail.com>
2024-08-19 12:04:05 -0600

    juasjuas

--- src/views/loginview.vue
+++ src/views/loginview.vue
@@ -99,7 +99,7 @@ const ingresarFormulario1 = async () => {
 
     console.log(result);
 
-    if (result.status === 200 && result.msg === "success") {
+    if (result.status === 200 ) {
       localStorage.setItem('token', result.data._token);
       userStore.setUsuario(result.data.usuario);
       router.push({ name: 'perfilusuario' });
